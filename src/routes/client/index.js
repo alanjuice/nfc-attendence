ae0af39f45b7382157d72207d9c0d7c33aaaed54
Alan <93069058+alanjuice@users.noreply.github.com>
2024-03-23 15:36:42 +0530

    add client registration

---
+++ src/routes/client/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const Joi = require("joi");
+
+const app = express.Router();
+const pool = require("../../database/pool");
+
+const clientSchema = Joi.object({
+  id: Joi.string().required().max(24),
+  name: Joi.string().required().max(64),
+  password: Joi.string().required().max(64),
+  email: Joi.string().required().max(32).email(),
+});
+
+app.post("/register", async (req, res) => {
+  const { error } = clientSchema.validate(req.body);
+  if (error) {
+    return res.status(400).json({ msg: error.message });
+  }
+  const { name, id, email, password } = req.body;
+  try {
+    const idUsed = await pool.query("SELECT * FROM CLIENT WHERE ID=$1", [id]);
+    console.log(idUsed);
+    if (idUsed.rowCount > 0) {
+      res.status(400).json({ msg: "id already exists" });
+      return;
+    }
+    await pool.query("INSERT INTO CLIENT VALUES($1,$2,$3,$4)", [
+      id,
+      name,
+      email,
+      password,
+    ]);
+    console.log("Client created " + id);
+    res.status(200).json({ msg: "client created" });
+  } catch (error) {
+    console.error("Error creating client:", error);
+    res.status(400).json({ msg: "Something went wrong" });
+  }
+});
+
+module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+const logger = require("./utils/logger");
+
 dotenv.config();
 const app = express();
 
+const clientRouter = require("./routes/client");
 const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(logger);
+app.use("/client", clientRouter);
 
 app.get("/status", (req, res) => {
   res.status(200).json({ status: "alive" });
